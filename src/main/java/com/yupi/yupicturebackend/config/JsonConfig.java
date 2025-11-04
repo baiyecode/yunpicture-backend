7ebf40730f459741a16d0507e28d1ad402394458
baiye <baiye062@gmail.com>
2025-11-04 15:48:16 +0800

    第 12 期 - 图片协同编辑 - 后端 1. WebSocket 实现图片协同编辑 2. Disruptor 优化图片协同编辑的性能

--- src/main/java/com/yupi/yupicturebackend/config/JsonConfig.java
+++ src/main/java/com/yupi/yupicturebackend/config/JsonConfig.java
@@ -13,11 +13,19 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 @JsonComponent
 public class JsonConfig {
 
+
+    /**
+     * 为什么这样做？ JavaScript 中的 Number 类型有精度限制，
+     * 无法安全地表示所有 64 位的 Java Long 值（超过 Number.MAX_SAFE_INTEGER 或低于 Number.MIN_SAFE_INTEGER 的值）。
+     * 通过将 Long 序列化为字符串，可以确保客户端 JavaScript 能够安全地接收和处理这些 ID，避免精度丢失。
+     */
+
     /**
      * 添加 Long 转 json 精度丢失的配置
      */
     @Bean
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        //调用 builder 的方法，明确指定不创建用于 XML 处理的 XmlMapper。虽然 false 通常是默认值，但显式指定可以增加清晰度。
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         SimpleModule module = new SimpleModule();
         // Long -> String
---
+++ src/main/java/com/yupi/yupicturebackend/manager/websocket/disruptor/PictureEditEventDisruptorConfig.java
@@ -0,0 +1,36 @@
+package com.yupi.yupicturebackend.manager.websocket.disruptor;
+
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+import com.lmax.disruptor.dsl.Disruptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+
+/**
+ * 图片编辑事件 disruptor 配置类
+ */
+@Configuration
+public class PictureEditEventDisruptorConfig {
+
+    @Resource
+    private PictureEditEventWorkHandler pictureEditEventWorkHandler;
+
+    @Bean("pictureEditEventDisruptor") //创建一个bean
+    public Disruptor<PictureEditEvent> messageModelRingBuffer() {
+        // ringBuffer 缓冲区的大小
+        int bufferSize = 1024 * 256;
+        Disruptor<PictureEditEvent> disruptor = new Disruptor<>(
+                PictureEditEvent::new, //放进缓冲区的数据类型
+                bufferSize,//缓冲区大小
+                //创建一个线程工厂。为 Disruptor 内部创建的工作线程设置名称前缀，方便在日志或线程分析工具中识别这些线程
+                ThreadFactoryBuilder.create().setNamePrefix("pictureEditEventDisruptor").build()
+        );
+        // 设置消费者
+        disruptor.handleEventsWithWorkerPool(pictureEditEventWorkHandler);
+        // 开启 disruptor
+        disruptor.start();
+        return disruptor;
+    }
+}
